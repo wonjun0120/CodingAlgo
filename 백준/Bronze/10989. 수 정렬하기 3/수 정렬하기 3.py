bcaa5cb11b425f13012a35de1ff52df5c4a769ac
Wonjun Choi <wonjundero@gmail.com>
2024-09-23 15:33:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7448 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+n = int(input())
 
 num_list = [0] * 10001
 for i in range(n):
     num_list[(int(input()))] += 1
 
 for i, num in enumerate(num_list):
-    if num > 0:
-        for _ in range(num):
-            print(i)
+    for _ in range(num):
+        print(i)
