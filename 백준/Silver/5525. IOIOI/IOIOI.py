3af96752a530ef4eb9052c7868c00f945472f1ec
Wonjun Choi <wonjundero@gmail.com>
2025-03-26 15:40:03 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,24 +1,24 @@
 import sys
-import collections
 
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-s = list(input().strip())
+s = input().strip()
 
-ioi = "I"
-for _ in range(n):
-    ioi += "OI"
-
+i = 0
+count = 0
 answer = 0
-for i in range(m):
-    if s[i] == "I":
-        if (i + len(ioi)) <= m:
+
+while i < m - 2:
+    if s[i] == 'I' and s[i + 1] == 'O' and s[i + 2] == 'I':
+        count += 1
+        if count == n:
             answer += 1
-            for j in range(len(ioi)):
-                if s[i + j] != ioi[j]:
-                    answer -= 1
-                    break
+            count -= 1  
+        i += 2  
+    else:
+        count = 0
+        i += 1
 
 print(answer)
