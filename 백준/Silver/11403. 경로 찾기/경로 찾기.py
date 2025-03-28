b6d3dcd0b472e0821195b6d355f32ab6c5965f9e
Wonjun Choi <wonjundero@gmail.com>
2025-03-28 12:16:46 +0900

    [Silver I] Title: 경로 찾기, Time: 92 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,12 +1,33 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+import collections
+
+input = sys.stdin.readline
+n = int(input())
+
+graph_li = [list(map(int, input().strip().split())) for _ in range(n)]
+graph = {k : [] for k in range(n)}
+for i in range(n):
+    for j in range(n):
+        if graph_li[i][j] == 1:
+            graph[i].append(j)
+
+answer = []
+for i in range(n):
+    stack = collections.deque(graph[i])
+    # print(i, graph_li)
+    # print(stack)
+    tmp = [0] * n
+    while stack:
+        cur = stack.pop()
+        if tmp[cur] == 1:
+            continue
+        
+        if tmp[cur] == 0:
+            tmp[cur] = 1
+            for it in graph[cur]:
+                stack.append(it)
+    answer.append(tmp)
+
+
+for li in answer:
+    print(" ".join([str(n) for n in li]))
