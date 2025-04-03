57218daf528357813d59d337fde36cb6cada6960
Wonjun Choi <wonjundero@gmail.com>
2025-04-03 13:22:23 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -6,35 +6,26 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 game_map = {}
 
-for _ in range(N):
+for _ in range(N + M):
     x, y = map(int, input().split())
-    game_map.setdefault(x, []).append(y)
+    game_map[x] = y
 
+queue = deque([(1, 0)])
+visited = [False] * 101
+visited[1] = True
 
-for _ in range(M):
-    u, v = map(int, input().split())
-    game_map.setdefault(u, []).append(v)
+while queue:
+    cur, time = queue.popleft()
 
-stack = deque([(1, 0)])
-is_visit= [False] * 101
-is_visit[1] = True
-# print(game_map)
-while stack:
-    cur, time = stack.popleft()
-    # print(cur, time)
-    to_move = cur
     for i in range(1, 7):
-        if cur + i > 100: continue
-        if not is_visit[cur + i]:
-            if cur + i == 100:
+        nxt = cur + i
+        if nxt > 100:
+            continue
+        move = game_map.get(nxt, nxt)
+
+        if not visited[move]:
+            if move == 100:
                 print(time + 1)
                 sys.exit(0)
-
-            if cur + i in game_map.keys():
-                for mv in game_map[cur + i]:
-                    stack.append((mv, time + 1))
-            else:
-                to_move = max(cur + i, to_move)
-            is_visit[cur + i] = True
-    
-    stack.append((to_move, time + 1))
+            visited[move] = True
+            queue.append((move, time + 1))
