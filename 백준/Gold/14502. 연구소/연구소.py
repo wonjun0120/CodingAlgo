3fe1f01c83671c24eeb3f8feeec84422de198e77
Wonjun Choi <wonjundero@gmail.com>
2025-07-02 13:24:55 +0900

    [Gold IV] Title: 연구소, Time: 348 ms, Memory: 114880 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,59 +1,45 @@
 import sys
-import itertools
 import collections
-import copy
 
 input = sys.stdin.readline
 
-def spread_virus(lab_map, virus):
-    stack = collections.deque(virus)
-
-    while stack:
-        i, j = stack.pop()
-        for x, y in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-            if 0 <= i + x < N and 0 <= j + y < M:
-                if lab_map[i + x][j + y] == 0:
-                    lab_map[i + x][j + y] = 2
-                    stack.append((i + x, j + y))
-    
-    return lab_map
-
-def cnt_room(lab_map):
-    cnt = 0
-    for li in lab_map:
-        cnt += len([x for x in li if x == 0])
-    
-    return cnt
-
-N,M = map(int, input().strip().split())
+N, M = map(int, input().strip().split())
 
 lab_map = []
-empty = []
-virus = []
+empty_loc = []
+virus_loc = []
 for i in range(N):
     li = list(map(int, input().strip().split()))
     lab_map.append(li)
-    empty += [(i, j) for j, el in enumerate(li) if el == 0]
-    virus += [(i, j) for j, el in enumerate(li) if el == 2]
+    for j, el in enumerate(li):
+        if el == 0: empty_loc.append((i, j))
+        if el == 2: virus_loc.append((i, j))
 
+def spread_virus(lab_map, virus_loc):
+    dq = collections.deque(virus_loc)
+
+    while dq:
+        x, y = dq.pop()
+        for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and lab_map[nx][ny] == 0:
+                lab_map[nx][ny] = 2
+                dq.append((nx, ny))
+
+    return sum([row.count(0) for row in lab_map])
 
 max_size = 0
-for item in itertools.combinations(empty, 3):
-    cur_map = copy.deepcopy(lab_map)
-    
-    for i, j in item:
-        cur_map[i][j] = 1
-    
-    spread_virus(cur_map, virus)
-    max_size = max(max_size, cnt_room(cur_map))
+def dfs(lab_map, empty_loc, idx, cnt):
+    global max_size
+    if cnt == 3:
+        max_size = max(max_size, spread_virus([r[:] for r in lab_map], virus_loc))
+        return
 
+    for i in range(idx, len(empty_loc)):
+        x, y = empty_loc[i]
+        lab_map[x][y] = 1
+        dfs(lab_map, empty_loc, i + 1, cnt + 1)
+        lab_map[x][y] = 0
+
+dfs(lab_map,empty_loc,0,0)
 print(max_size)
-
-
-
-
-
-
-
-
-
