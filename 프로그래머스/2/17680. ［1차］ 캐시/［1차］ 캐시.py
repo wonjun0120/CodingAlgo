37b6f5bbca46cb967e478f26b206c1f846202c36
Wonjun Choi <wonjundero@gmail.com>
2025-10-02 09:25:48 +0900

    [level 2] Title: [1차] 캐시, Time: 165.45 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,33 +1,3 @@
-from collections import deque
-
 def solution(cacheSize, cities):
-    cache = deque([])
-    
     answer = 0
-    
-    for city in cities:
-        city = city.lower()
-        
-        is_in = False
-        idx = -1
-        for i, c in enumerate(cache):
-            if c == city:
-                is_in = True
-                idx = i
-                break
-        
-        if is_in:
-            answer += 1
-            cache.remove(city)
-            cache.append(city)
-        
-        else:
-            answer += 5
-            cache.append(city)
-        
-        if len(cache) > cacheSize:
-            cache.popleft()
-
-
     return answer
-    
