dc370d31f2415fb72b4cf78e8eccb14e17b2e008
Wonjun Choi <wonjundero@gmail.com>
2025-10-13 10:19:40 +0900

    [Gold V] Title: LCS, Time: 428 ms, Memory: 57628 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+ls = input().strip()
+rs = input().strip()
+
+n, m = len(ls), len(rs)
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if ls[i - 1] == rs[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[n][m])
